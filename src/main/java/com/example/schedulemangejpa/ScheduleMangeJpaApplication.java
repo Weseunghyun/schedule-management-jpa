dcde6ad81a51b244623fe6a0b3c33dbd76d4245c
Weseunghyun <bible091192@gmail.com>
2024-11-13 13:19:51 +0900

    init commit

---
+++ src/main/java/com/example/schedulemangejpa/ScheduleMangeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemangejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleMangeJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleMangeJpaApplication.class, args);
+    }
+
+}
